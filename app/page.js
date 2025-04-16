0e6b6fcfb218e5970b4315617b16e3a58a649069
rohit1b <rohitthakurrt365@gmail.com>
2025-04-17 00:33:03 +0530

    contact

--- app/page.js
+++ app/page.js
@@ -141,7 +141,7 @@ export default function Home() {
           </div>
         </section>
 
-        {/* Contact Section */}
+        {/* 8954188201 */}
         <section id="contact" className="py-12 md:py-24 lg:py-32">
           <div className="container px-4 md:px-6">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-8">Contact</h2>
